c43c4db71956d6b4909d402d3b55dc67c2e495e8
Joshua <Joshua.Doll@online.de>
2020-03-04 20:57:09 +0100

    Update Paintbums.java

--- src/main/java/de/cosmiqglow/paintbums/Paintbums.java
+++ src/main/java/de/cosmiqglow/paintbums/Paintbums.java
@@ -1,16 +1,26 @@
 package de.cosmiqglow.paintbums;
 
+import de.cosmiqglow.fluctus.ScheduledGamePhaseSeries;
 import de.cosmiqglow.paintbums.listeners.CancelListener;
 import de.cosmiqglow.paintbums.listeners.ChatListener;
 import de.cosmiqglow.paintbums.listeners.CommandListener;
+import de.cosmiqglow.paintbums.phases.IngamePhase;
+import de.cosmiqglow.paintbums.phases.LobbyPhase;
+import de.cosmiqglow.paintbums.phases.RestartPhase;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Paintbums extends JavaPlugin {
+
+    private final ScheduledGamePhaseSeries mainSeries = new ScheduledGamePhaseSeries(this);
 
     @Override
     public void onEnable() {
         loadConfig();
         registerListeners();
+
+        mainSeries.add(new LobbyPhase(this));
+        mainSeries.add(new IngamePhase(this, 60*5));
+        mainSeries.add(new RestartPhase(this, 30));
     }
 
     @Override
@@ -19,14 +29,14 @@ public class Paintbums extends JavaPlugin {
     }
 
     private void loadConfig() {
-        getConfig().addDefault("chat-options.use-provided-chat", true );
+        getConfig().addDefault("chat-options.provided-chat", true );
         getConfig().options().copyDefaults(true);
         saveConfig();
         reloadConfig();
     }
 
     private void registerListeners() {
-        if (getConfig().getBoolean("Chat-Options.provided-chat"))
+        if (getConfig().getBoolean("chat-options.provided-chat"))
             this.getServer().getPluginManager().registerEvents(new ChatListener(), this);
 
         this.getServer().getPluginManager().registerEvents(new CancelListener(), this);
